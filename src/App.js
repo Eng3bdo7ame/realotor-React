e5d472bad90763d05a49f40ff643bddadec634ef
Eng_3bdo_7amed <3bdo.7amed.com@gmail.com>
2024-06-15 14:27:56 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import "./App.css";
+// import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Offer from "./pages/Offer";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
     <>
-      <h1 className="bg-red-600 text-3xl font-bold underline head"> hello </h1>{" "}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />{" "}
+          <Route path="/offer" element={<Offer />} />{" "}
+          <Route path="/signin" element={<SignIn />} />{" "}
+          <Route path="/signup" element={<SignUp />} />{" "}
+          <Route path="/profile" element={<Profile />} />{" "}
+        </Routes>{" "}
+      </Router>{" "}
     </>
   );
 }
